a39155cabbe58fb64ec40af125dd46a782325e21
Phạm Tuấn <phamtuan06112003@gmail.com>
2025-03-11 11:22:52 +0700

    Making Level 02

--- Assets/_Game/Scripts/Pickup/FruitPickup.cs
+++ Assets/_Game/Scripts/Pickup/FruitPickup.cs
@@ -6,12 +6,14 @@ public class FruitPickup : MonoBehaviour
 {
     [SerializeField] private int amount = 1;
     [SerializeField] private int countFruit = 0;
+    [SerializeField] private GameObject effectFruit;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             countFruit++;
             Destroy(gameObject);
+            Instantiate(effectFruit, transform.position, Quaternion.identity);
 
             //AudioManager.instance.allSFXPlayPitched(9);
         }
---
+++ Assets/_Game/Scripts/UI/EndLevel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("flagActive", true);
+            StartCoroutine(Delay());
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(nextScene);
+    }
+}
